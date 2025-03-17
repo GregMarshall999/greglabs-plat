0c2da01c18b9b3890a2b260f72776aa9551e21b1
Greg <gregory.marshall999@gmail.com>
2025-03-17 16:25:37 +0100

    add movie

--- 01_movie-rating-app-1/src/App.vue
+++ 01_movie-rating-app-1/src/App.vue
@@ -1,7 +1,78 @@
 <template>
-	<!-- This is where your template goes	-->
 	<div class="container">
 		<h1>Movies</h1>
+
+    <button class="add-movie-button" @click="showMovieModal">Add Movie</button>
+
+    <div v-if="displayMovieModal" class="movie-modal">
+      <form class="movie-form" @submit.prevent="createMovie">
+        <div class="form-item">
+          <label>Name</label>
+          <input 
+            v-model="newMovie.name"
+            class="form-input" 
+            type="text" 
+            required
+          />
+        </div>
+
+        <div class="form-item">
+          <label>Description</label>
+          <textarea v-model="newMovie.description" class="form-input form-text"></textarea>
+        </div>
+        
+        <div class="form-item">
+          <label>Image</label>
+          <input 
+            v-model="newMovie.image"
+            class="form-input" 
+            type="text"
+            required
+          />
+        </div>
+
+        <div class="form-item">
+          <label>Genres</label>
+          <div class="genres-input-container">
+            <div class="genres-list">
+              <span 
+                v-for="genre in movieGenres" 
+                :key="genre"
+                class="genre-tag"
+                :class="{ 'selected': newMovie.genres.includes(genre) }"
+                @click="toggleGenre(genre)"
+              >
+                {{ genre }}
+              </span>
+            </div>
+            <div class="new-genre-input">
+              <input 
+                v-model="newGenre" 
+                class="form-input" 
+                type="text"
+                placeholder="New genre... (space to add)"
+                @keypress.space="addNewGenre"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="theater-checkbox">
+          <input 
+            v-model="newMovie.inTheaters"
+            id="theater-checkbox" 
+            class="checkbox" 
+            type="checkbox"
+          />
+          <label for="theater-checkbox">In theaters</label>
+        </div>
+
+        <div class="form-actions">
+          <button class="form-button gray" type="button" @click="closeMovieModal">Cancel</button>
+          <button class="form-button blue" type="submit">Create</button>
+        </div>
+      </form>
+    </div>
 
 		<ul class="movies">
 			<li 
@@ -13,7 +84,7 @@
           <StarIcon class="big-star" />
           <label>{{ movie.rating }}</label>
         </div>
-				<img :src="movie.image" :alt="movie.name" />
+				<img :src="movie.image" :alt="movie.name" crossorigin="anonymous"/>
 
 				<div class="movie-info">
 					<div class="movie-info-content">
@@ -55,15 +126,10 @@
 
 <script setup>
 import { items } from "./movies.json";
-/*
- This is an Icon that you can use to represent the stars if you like
- otherwise you could just use a simple ⭐️ emoji, or * character.
-*/
 import { StarIcon } from "@heroicons/vue/24/solid";
-import { computed, ref } from "vue";
+import { computed, reactive, ref } from "vue";
 
 const movies = ref(items);
-
 const movieRatings = computed(() => 
   movies.value.map(movie => {
     const rating = [];
@@ -79,6 +145,13 @@ const movieRatings = computed(() =>
     return rating;
   })
 );
+const movieGenres = computed(() => {
+  let allGenres = movies.value.flatMap(movie => movie.genres);
+
+  allGenres = [...allGenres, ...newGenres.value];
+
+  return [...new Set(allGenres)];
+});
 
 const clickable = computed(() => 
   movieRatings.value.map(ratings => {
@@ -101,6 +174,52 @@ const clickable = computed(() =>
 const rateMovie = (movieId, starNum) => {
   movies.value.find(movie => movie.id == movieId).rating = starNum;
 }
+
+const displayMovieModal = ref(false);
+const showMovieModal = () => {
+  displayMovieModal.value = true;
+}
+const closeMovieModal = () => {
+  displayMovieModal.value = false;
+}
+
+const newMovie = reactive({
+  name: '',
+  description: '',
+  image: '',
+  genres: [],
+  inTheaters: false
+});
+const newGenre = ref('');
+const newGenres = ref([]);
+const toggleGenre = (genre) => {
+  if (newMovie.genres.includes(genre)) {
+    newMovie.genres = newMovie.genres.filter(g => g !== genre);
+  } else {
+    newMovie.genres.push(genre);
+  }
+}
+const addNewGenre = () => {
+  if (newGenre.value.trim() && !newMovie.genres.includes(newGenre.value.trim())) {
+    newGenres.value.push(newGenre.value.trim());
+    newGenre.value = '';
+  }
+}
+const createMovie = () => {
+  const movie = { ...newMovie };
+
+  movies.value.push(movie);
+
+  newMovie.name = '';
+  newMovie.description = '';
+  newMovie.image = '';
+  newMovie.genres = [];
+  newMovie.inTheaters = false;
+
+  newGenres.value = [];
+
+  closeMovieModal()
+}
 </script>
 
 <style>
@@ -120,12 +239,154 @@ body {
   max-width: 1200px;
   margin: 0 auto;
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
 }
 
 h1 {
   color: #fff;
   font-size: 2rem;
-  margin-bottom: 2em;
+  margin-bottom: 1em;
+}
+
+.add-movie-button {
+  background-color: #0095c6;
+  color: #fff;
+  font-size: medium;
+  width: fit-content;
+  align-self: flex-end;
+  border: none;
+  border-radius: 5px;
+  padding: 0.5em;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0077a3;
+  }
+}
+
+.movie-modal {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(5px);
+}
+
+.movie-form {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5em;
+  background-color: #1a2438;
+  padding: 1em;
+  border-radius: 5px;
+  width: 30%;
+}
+
+.form-item {
+  display: flex;
+  flex-direction: column;
+  gap: .2em;
+}
+
+.form-input {
+  background-color: #111826;
+  border: 2px solid #2c3c56;
+  border-radius: 5px;
+  color: #fff;
+  padding: .2em .5em;
+  width: 100%;
+}
+
+.form-text {
+  min-height: 100px;
+}
+
+.checkbox {
+  margin-right: .5em;
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+}
+
+.theater-checkbox {
+  display: flex;
+  align-items: center;
+  position: relative;
+  padding-left: 35px;
+  cursor: pointer;
+  user-select: none;
+}
+
+.theater-checkbox label {
+  cursor: pointer;
+}
+
+.theater-checkbox label::before {
+  content: '';
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 20px;
+  height: 20px;
+  background-color: #111826;
+  border: 2px solid #2c3c56;
+  border-radius: 4px;
+}
+
+.theater-checkbox input:checked + label::before {
+  background-color: #0095c6;
+  border-color: #0095c6;
+}
+
+.theater-checkbox input:checked + label::after {
+  content: '';
+  position: absolute;
+  left: 7px;
+  top: 3px;
+  width: 6px;
+  height: 12px;
+  border: solid white;
+  border-width: 0 2px 2px 0;
+  transform: rotate(45deg);
+}
+
+.form-actions {
+  display: flex;
+  justify-content: space-between;
+}
+
+.form-button {
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: .5em;
+  cursor: pointer;
+}
+
+.gray {
+  background-color: #7a7a7a;
+
+  &:hover {
+    background-color: #575757;
+  }
+}
+
+.blue {
+  background-color: #0095c6;
+
+  &:hover {
+    background-color: #0077a3;
+  }
 }
 
 .movies {
@@ -264,5 +525,36 @@ p {
   .movie img {
     height: 300px;
   }
+}
+
+.genre-tag {
+  background-color: #2c3c56;
+  color: #fff;
+  padding: 4px 12px;
+  border-radius: 999px;
+  font-size: 0.8rem;
+  margin: 4px;
+  cursor: pointer;
+  display: inline-block;
+}
+
+.genre-tag.selected {
+  background-color: #0095c6;
+}
+
+.genres-input-container {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+.genres-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+}
+
+.new-genre-input {
+  margin-top: 5px;
 }
 </style>
